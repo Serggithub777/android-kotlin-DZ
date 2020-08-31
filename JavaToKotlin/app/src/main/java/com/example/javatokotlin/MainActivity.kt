20d5b2163d7f47bc0a9f5a3e9a34b1f944137140
SergSerg <sergio.ne@inbox.ru>
2020-08-31 14:48:14 +0600

    Добавил обработку нажатия кнопки в MainActivity.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/MainActivity.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.javatokotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewHelloKotlin = findViewById<TextView>(R.id.text_view_hello_kotlin)
+        val buttonSayHelloKotlin = findViewById<Button>(R.id.button_say_hello_kotlin)
+
+        buttonSayHelloKotlin.setOnClickListener {
+            textViewHelloKotlin.text = "Hello Kotlin!"
+        }
     }
 }
