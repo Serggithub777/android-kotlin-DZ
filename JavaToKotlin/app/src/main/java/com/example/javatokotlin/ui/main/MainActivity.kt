20e91cb8170d80d30b49698853960ac333d0b6e4
SergSerg <sergio.ne@inbox.ru>
2020-09-04 23:09:08 +0600

    Создал обработку ресайклера в MainActivity.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
@@ -2,12 +2,28 @@ package com.example.javatokotlin.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.javatokotlin.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
-        }
+        var viewModel: MainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+
+        rv_notes.layoutManager = GridLayoutManager(this, 2)
+
+        val adapter = NotesRVAdapter()
+        rv_notes.adapter = adapter
+
+        viewModel.viewState().observe(this, Observer { state ->
+            state?.let { adapter.notes = state.notes }
+
+        })
     }
+}
---
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/NotesRVAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.javatokotlin.ui.main
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.javatokotlin.R
+import com.example.javatokotlin.data.model.Note
+import kotlinx.android.synthetic.main.item_note.view.*
+
+class NotesRVAdapter() : RecyclerView.Adapter<NotesRVAdapter.ViewHolder>() {
+
+     var notes: List<Note> = listOf()
+     set(value) {
+         field = value
+         notifyDataSetChanged()
+     }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+            ViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                            R.layout.item_note,
+                            parent,
+                            false
+                    )
+            )
+
+    override fun getItemCount()= notes.size
+
+    override fun onBindViewHolder(vh: ViewHolder, pos: Int) = vh.bind(notes[pos])
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bind(note: Note) = with(itemView) {
+            tv_title.text = note.title
+            tv_text.text = note.text
+            setBackgroundColor(note.color)
+        }
+    }
+}
