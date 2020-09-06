5ded84b0cb77f8fd1916f9369bef7f66fb9dfece
SergSerg <sergio.ne@inbox.ru>
2020-09-06 16:07:01 +0600

    Добавил Афи в разметку activity_main.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.javatokotlin.R
+import com.example.javatokotlin.ui.note.NoteActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,12 +20,17 @@ class MainActivity : AppCompatActivity() {
 
         rv_notes.layoutManager = GridLayoutManager(this, 2)
 
-        val adapter = NotesRVAdapter()
+        val adapter = NotesRVAdapter(){
+            NoteActivity.start(this, it)
+        }
         rv_notes.adapter = adapter
 
         viewModel.viewState().observe(this, Observer { state ->
             state?.let { adapter.notes = state.notes }
 
         })
+        fab.setOnClickListener {
+            NoteActivity.start(this)
+        }
     }
 }
