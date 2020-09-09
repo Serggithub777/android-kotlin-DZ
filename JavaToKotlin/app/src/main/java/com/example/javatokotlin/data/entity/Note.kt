0d917ededfacf6e522937c2c316f6a5643baf8b8
SergSerg <sergio.ne@inbox.ru>
2020-09-09 19:03:57 +0600

    Внес изменения в класс Note.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/data/entity/Note.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/data/entity/Note.kt
@@ -5,13 +5,13 @@ import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
-data class Note (
-        val id: String,
-        val title: String,
-        val text: String,
+data class Note(
+        val id: String = "",
+        val title: String = "",
+        val text: String = "",
         val color: Color = Color.WHITE,
         val lastChanged: Date = Date()
-) : Parcelable {
+): Parcelable {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -21,6 +21,7 @@ data class Note (
         if (id != other.id) return false
         return true
     }
+
     enum class Color {
         WHITE,
         YELLOW,
@@ -30,5 +31,3 @@ data class Note (
         VIOLET,
     }
 }
-
-
