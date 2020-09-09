bc90ef8429e445de04eca1f7b97e986397a2a2d2
SergSerg <sergio.ne@inbox.ru>
2020-09-09 19:37:06 +0600

    Внес изменения в класс MainActivity. Создал класс NoteViewState.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/main/MainActivity.kt
@@ -6,31 +6,41 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.javatokotlin.R
+import com.example.javatokotlin.data.entity.Note
+import com.example.javatokotlin.ui.base.BaseActivity
+import com.example.javatokotlin.ui.base.BaseViewModel
 import com.example.javatokotlin.ui.note.NoteActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<List<Note>?, MainViewState>() {
+
+    override val viewModel: BaseViewModel<List<Note>?, MainViewState> by lazy {
+        ViewModelProviders.of(this).get(MainViewModel::class.java)
+    }
+
+    override val layoutRes = R.layout.activity_main
+
+    lateinit var adapter: NotesRVAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        val viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-
-        rv_notes.layoutManager = GridLayoutManager(this, 2)
-
-        val adapter = NotesRVAdapter(){
-            NoteActivity.start(this, it)
+        rv_notes.layoutManager =
+                GridLayoutManager(this, 2)
+        adapter = NotesRVAdapter { note ->
+            NoteActivity.start(this, note.id)
         }
         rv_notes.adapter = adapter
-
-        viewModel.viewState().observe(this, Observer { state ->
-            state?.let { adapter.notes = state.notes }
-
-        })
         fab.setOnClickListener {
             NoteActivity.start(this)
         }
     }
+
+    override fun renderData(data: List<Note>?) {
+        data?.let {
+            adapter.notes = it
+        }
+    }
+
 }
+
