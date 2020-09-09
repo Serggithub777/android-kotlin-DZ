35dedd5e6ddb9ec529df261401d3b500a96dc7d3
SergSerg <sergio.ne@inbox.ru>
2020-09-09 19:38:45 +0600

    Внес изменения в класс NoteViewModel.

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/note/NoteViewModel.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/note/NoteViewModel.kt
@@ -3,12 +3,29 @@ package com.example.javatokotlin.ui.note
 import androidx.lifecycle.ViewModel
 import com.example.javatokotlin.data.NoteRepository
 import com.example.javatokotlin.data.entity.Note
+import com.example.javatokotlin.data.model.NoteResult
+import com.example.javatokotlin.ui.base.BaseViewModel
 
-class NoteViewModel: ViewModel() {
+class NoteViewModel : BaseViewModel<Note?, NoteViewState>() {
+
     private var pendingNote: Note? = null
 
     fun save(note: Note) {
         pendingNote = note
+    }
+
+    fun loadNote(noteId: String) {
+        NoteRepository.getNoteById(noteId).observeForever { result ->
+            result ?: return@observeForever
+            when (result) {
+                is NoteResult.Success<*> -> {
+                    viewStateLiveData.value = NoteViewState(note = result.data as? Note)
+                }
+                is NoteResult.Error -> {
+                    viewStateLiveData.value = NoteViewState(error = result.error)
+                }
+            }
+        }
     }
 
     override fun onCleared() {
@@ -16,4 +33,5 @@ class NoteViewModel: ViewModel() {
             NoteRepository.saveNote(it)
         }
     }
+
 }
