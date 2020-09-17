13ef1b40ca0dbd5aa7e9cc43bc23218ca9f0fc57
SergSerg <sergio.ne@inbox.ru>
2020-09-17 10:50:35 +0600

    Внес изменения в NoteViewModel.kt

--- JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/note/NoteViewModel.kt
+++ JavaToKotlin/app/src/main/java/com/example/javatokotlin/ui/note/NoteViewModel.kt
@@ -6,31 +6,41 @@ import com.example.javatokotlin.data.entity.Note
 import com.example.javatokotlin.data.model.NoteResult
 import com.example.javatokotlin.ui.base.BaseViewModel
 
-class NoteViewModel : BaseViewModel<Note?, NoteViewState>() {
+class NoteViewModel(val notesRepository: NotesRepository) : BaseViewModel<NoteViewState.Data, NoteViewState>() {
 
-    private var pendingNote: Note? = null
+    private val pendingNote: Note?
+        get() = viewStateLiveData.value?.data?.note
+
 
     fun save(note: Note) {
-        pendingNote = note
+        viewStateLiveData.value = NoteViewState(NoteViewState.Data(note = note))
     }
 
     fun loadNote(noteId: String) {
-        NoteRepository.getNoteById(noteId).observeForever { result ->
+        notesRepository.getNoteById(noteId).observeForever { result ->
             result ?: return@observeForever
             when (result) {
-                is NoteResult.Success<*> -> {
-                    viewStateLiveData.value = NoteViewState(note = result.data as? Note)
-                }
-                is NoteResult.Error -> {
-                    viewStateLiveData.value = NoteViewState(error = result.error)
-                }
+                is NoteResult.Success<*> -> viewStateLiveData.value = NoteViewState(NoteViewState.Data(note = result.data as? Note))
+                is NoteResult.Error -> viewStateLiveData.value = NoteViewState(error = result.error)
             }
         }
     }
 
     override fun onCleared() {
         pendingNote?.let {
-            NoteRepository.saveNote(it)
+            notesRepository.saveNote(it)
+        }
+    }
+
+    fun deleteNote() {
+        pendingNote?.let {
+            notesRepository.deleteNote(it.id).observeForever { result ->
+                result ?: return@observeForever
+                when (result) {
+                    is NoteResult.Success<*> -> viewStateLiveData.value = NoteViewState(NoteViewState.Data(isDeleted = true))
+                    is NoteResult.Error -> viewStateLiveData.value = NoteViewState(error = result.error)
+                }
+            }
         }
     }
 
